22f4d9bc43b56deb7a2f69bf5fe7f9e5f26ea8ca
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-09-29 19:24:32 -0300

    refactor: jest ignores mocks file

--- server/jest.config.js
+++ server/jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFiles: ['dotenv/config'],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/__tests__/mocks/"
+  ],
 };
