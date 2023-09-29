06ec61f44130b70c9a7ade82a8f065da9dd13e8f
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-09-29 19:46:30 -0300

    tests: createMocks updaet, get and default product

--- server/__tests__/mocks/product.ts
+++ server/__tests__/mocks/product.ts
@@ -1,3 +1,12 @@
+export const defaultProduct = {
+  id: "123456789",
+  name: "Mousepad",
+  price: 255.55,
+  image: "fakeurl",
+  discountRate: 16,
+  date: Date.now(),
+}
+
 const newProduct = {
   name: "Mousepad",
   price: 255.55,
@@ -5,6 +14,21 @@ const newProduct = {
   discountRate: 16,
 }
 
-export const keys = ["id", "name", "price", "image", "discountRate"];
+const getProduct = {
+  id: "123456",
+  name: "Mousepad",
+}
+
+export const updateProduct = {
+  id: "idProduct",
+  body: {
+    name: "newName",
+    price: "newPrice",
+    image: "newImage",
+    discountRate: "newDiscountRate",
+  }
+}
+
+export const keys = ["id", "name", "price", "image", "discountRate", "date"];
 
 export default newProduct;
