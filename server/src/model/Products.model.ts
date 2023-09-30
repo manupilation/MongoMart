07efad58de90fbf11299cf02e9044857f456a956
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-09-30 05:02:01 -0300

    feat: Products.model methods

--- server/src/model/Products.model.ts
+++ server/src/model/Products.model.ts
@@ -1,4 +1,4 @@
-import product from "../types/Product";
+import product, { updateProduct } from "../types/Product";
 import { ProductDBModel } from "./Product"
 
 class ProductModel {
@@ -16,6 +16,30 @@ class ProductModel {
       ...create,
       price: Number(create.price),
     };
+  }
+
+  async getProducts() {
+    const products = await this.productAccessDb.find().limit(5);
+
+    return products;
+  }
+
+  async getProduct(id: string) {
+    const product = await this.productAccessDb.findOne({id: id});
+
+    return product;
+  }
+
+  async updateProduct(product: updateProduct) {
+    const updateProduct = await this.productAccessDb.findOneAndUpdate({ id: product.id }, { ...product.body });
+
+    return updateProduct;
+  }
+
+  async deleteProduct(id: string) {
+    await this.productAccessDb.findOneAndDelete({id});
+
+    return "ok";
   }
 }
 
