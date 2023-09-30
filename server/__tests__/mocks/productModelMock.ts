c78eb8355a482140c46ff75efef87bafe266fadd
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-09-30 19:25:07 -0300

    tests-feat: mock get methods

--- server/__tests__/mocks/productModelMock.ts
+++ server/__tests__/mocks/productModelMock.ts
@@ -1,9 +1,18 @@
-import { defaultProduct } from "./product";
+import newProduct, { defaultProduct } from "./product";
 
 class ProductModelMock {
   createProduct(req: Request, res: Response) {
     return defaultProduct;
   }
+
+  getProducts(req: Request, res: Response) {
+    return new Array(5).fill(newProduct);
+  }
+
+  getProduct(req: Request, res: Response) {
+    return newProduct;
+  }
+
 }
 
 export default ProductModelMock;
