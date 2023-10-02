c876d12a92df25ad9daf8c036ca549b37132d2e0
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-10-02 00:27:34 -0300

    refactor: all routes product using resolver helper

--- server/src/controller/ProductsController.ts
+++ server/src/controller/ProductsController.ts
@@ -1,7 +1,8 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import ProductModel from "../model/Productsmodel";
 import product, { updateProduct } from "../types/Product";
 import HttpStatus from "../enum/HTTPStatus";
+import resolver from "../helper/Resolver";
 
 class ProductController {
   private productModel: ProductModel;
@@ -10,43 +11,39 @@ class ProductController {
     this.productModel = new ProductModel();
   }
 
-  async createProduct(req: Request<{body: product}>, res: Response): Promise<void> {
-    try {
-      const { body } = req;
-      const create = await this.productModel.createProduct(body as product);
-      res.status(HttpStatus.Created).json({product: create});
-    } catch (err) {
-      res.status(HttpStatus.InternalServerError).json({error: err.message});
-    }
+  createProduct = resolver(async (req: Request<{body: product}>, res: Response, _: NextFunction): Promise<void> => {
+    const { body } = req;
+    const create = await this.productModel.createProduct(body as product);
 
-  }
+    res.status(HttpStatus.Created).json({product: create});
+  });
 
-  async getProducts(_req: Request<{body: product}>, res: Response): Promise<void> {
+  getProducts = resolver(async (_req: Request<{body: product}>, res: Response, _: NextFunction): Promise<void> =>{
     const products = await this.productModel.getProducts();
 
     res.status(HttpStatus.OK).json({products: products });
-  }
+  });
 
-  async getProduct(req: Request<{body: {id: string}}>, res: Response): Promise<void> {
+   getProduct = resolver(async (req: Request<{body: {id: string}}>, res: Response, _: NextFunction): Promise<void> => {
     const { id } = req.body;
     const product = await this.productModel.getProduct(id);
 
     res.status(HttpStatus.OK).json({product: product });
-  }
+  });
 
-  async updateProduct(req: Request<{body: updateProduct}>, res: Response): Promise<void> {
+  updateProduct = resolver( async(req: Request<{body: updateProduct}>, res: Response, _: NextFunction): Promise<void> => {
     const { body } = req;
     const product = await this.productModel.updateProduct(body as updateProduct);
 
     res.status(HttpStatus.OK).json({product: product });
-  }
+  });
 
-  async deleteProduct(req: Request<{body: {id: string}}>, res: Response): Promise<void> {
+  deleteProduct = resolver(async (req: Request<{body: {id: string}}>, res: Response, _: NextFunction): Promise<void> => {
     const { id } = req.body;
     await this.productModel.deleteProduct(id);
 
     res.status(HttpStatus.OK).json({ excluded: "ok" });
-  }
+  });
 }
 
 export default ProductController;
