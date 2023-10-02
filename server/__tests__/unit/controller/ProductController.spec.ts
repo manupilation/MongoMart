a392e61963526e63eb051ac26fad3cefa8ba8003
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-10-02 00:21:02 -0300

    tests: add nextfunction to tests

--- server/__tests__/unit/controller/ProductController.spec.ts
+++ server/__tests__/unit/controller/ProductController.spec.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { NextFunction, Response } from "express";
 import ProductModelMock from "../../mocks/productModelMock";
 import ProductController from "../../../src/controller/ProductsController"
 import newProduct, { defaultProduct } from "../../mocks/product";
@@ -6,12 +6,13 @@ import product from "../../../src/types/Product";
 
 
 describe('Tests ProductController Class', () => {
-  let req: any, res: Response;
+  let req: any, res: Response, next: NextFunction;
   let productController: ProductController;
 
   beforeAll(() => {
     req = {} as any;
     res = {} as Response;
+    next = () => {};
   });
 
   beforeEach(() => {
@@ -25,14 +26,14 @@ describe('Tests ProductController Class', () => {
 
   test('Tests controller CreateProduct', async () => {
     req.body = newProduct;
-    await productController.createProduct(req, res);
+    await productController.createProduct(req, res, next);
 
     expect(res.status).toBeCalledWith(201);
     expect(res.json).toBeCalledWith({ product: defaultProduct });
   });
 
   test('Tests controller getProducts', async () => {
-    await productController.getProducts(req, res);
+    await productController.getProducts(req, res, next);
 
     expect(res.status).toBeCalledWith(200);
     expect(res.json).toBeCalledWith({ products: new Array(5).fill(newProduct) });
@@ -40,7 +41,7 @@ describe('Tests ProductController Class', () => {
 
   test('Tests controller getProduct', async () => {
     req.params = "fakeId"
-    await productController.getProduct(req, res);
+    await productController.getProduct(req, res, next);
 
     expect(res.status).toBeCalledWith(200);
     expect(res.json).toBeCalledWith({ product: newProduct });
@@ -48,7 +49,7 @@ describe('Tests ProductController Class', () => {
 
   test('Tests controller updateProduct', async () => {
     req.body = { id: "fakeId", product: {} }
-    await productController.updateProduct(req, res);
+    await productController.updateProduct(req, res, next);
 
     expect(res.status).toBeCalledWith(200);
     expect(res.json).toBeCalledWith({ product: defaultProduct });
@@ -56,7 +57,7 @@ describe('Tests ProductController Class', () => {
   
   test('Tests controller deleteProduct', async () => {
     req.body = "fakeId"
-    await productController.deleteProduct(req, res);
+    await productController.deleteProduct(req, res, next);
 
     expect(res.status).toBeCalledWith(200);
     expect(res.json).toBeCalledWith({ excluded: "ok" });
