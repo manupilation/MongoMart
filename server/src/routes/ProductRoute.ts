91efaac43a67f732242401a9904d08b5acaec296
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-10-02 00:28:31 -0300

    refactor: route req res types

--- server/src/routes/ProductRoute.ts
+++ server/src/routes/ProductRoute.ts
@@ -1,11 +1,12 @@
-import express from "express";
+import { Request, Response } from "express";
+import express, { NextFunction } from "express";
 import ProductController from "../controller/ProductsController";
 
 const productController = new ProductController();
 const productRoute = express.Router();
 
-productRoute.post("/product", (req: any, res: any) => {
-    return productController.createProduct(req, res);
+productRoute.post("/product", (req: Request<any>, res: Response<any>, _: NextFunction) => {
+    return productController.createProduct(req, res, _);
   });
 
 export default productRoute;
