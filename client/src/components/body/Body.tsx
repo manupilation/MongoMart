a5aad6111e8bb539f432547902283d7b9151976d
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-10-03 18:02:07 -0300

    refactor: now body fetchs products

--- client/src/components/body/Body.tsx
+++ client/src/components/body/Body.tsx
@@ -4,8 +4,27 @@ import ModalForm from "../modalForm/modalForm";
 import EditProductContext from "../../context/editProductContext";
 import EditProductModal from "../editProductModal/editProductModal";
 import "./Body.css";
+import { useContext, useEffect } from "react";
+import { globalContext } from "../../context/globalContext";
+import useProductThreatment from "../../hooks/UseProductThreatment";
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 const Body = () => {
+  const {setProducts} = useContext(globalContext);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const data = await fetch(backendUrl + "products");
+      const {products} = await data.json();
+
+      const threatmentProducts = useProductThreatment(products);
+      setProducts(threatmentProducts);
+    }
+
+    fetchProducts();
+  }, [setProducts]);
+
   return (
     <EditProductContext>
       <div className="bodyWrapper">
