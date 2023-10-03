f7c353cd25ca12efe73a3f2ebaf5570b31fcd974
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-10-02 23:22:03 -0300

    feat: SearchBar component

---
+++ client/src/components/searchBar/SearchBar.tsx
@@ -0,0 +1,11 @@
+import "./SearchBar.css";
+
+const SearchBar = () => {
+  return (
+    <div className="inputWrapper">
+      <input type="text" name="" id="" placeholder='Filter our products' className="inputBar"/>
+    </div>
+  );
+};
+
+export default SearchBar;
