dc4d6724a0c0934019db7ab6786b5a34f735d881
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-10-03 03:41:34 -0300

    feat: uses global context on app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import Body from "./components/body/Body";
 import Header from "./components/header/Header";
+import GlobalContext from "./context/globalContext";
 
 function App() {
   return (
-    <>
+    <GlobalContext>
       <Header />
       <Body />
-    </>
+    </GlobalContext>
   );
 }
 
