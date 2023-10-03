1f1a5e5cd0e87d7fefe9ad92f6c774f314afa81c
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-10-03 03:42:08 -0300

    feat: add products button

--- client/src/components/addProduct/addProduct.tsx
+++ client/src/components/addProduct/addProduct.tsx
@@ -1,9 +1,23 @@
+import { useContext } from "react";
 import "./addProduct.css";
+import { globalContext } from "../../context/globalContext";
 
 const AddProduct = () => {
+  const {setIsCreating} = useContext(globalContext);
+
+  function handleIsEditing(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    event.preventDefault();
+
+    setIsCreating(true);
+  }
+
   return (
     <div className="addButtonWrapper">
-      <button type="button" className="addButton">+</button>
+      <button 
+        type="button" 
+        className="addButton"
+        onClick={(e) => handleIsEditing(e)}
+      >+</button>
     </div>
   )
 }
