15f03d104d5bf5470f510fe41573d7869fd1a1fa
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-10-03 03:42:33 -0300

    feat: modal form

--- client/src/components/body/Body.tsx
+++ client/src/components/body/Body.tsx
@@ -1,10 +1,12 @@
 import ProductList from "../productList/ProductList";
 import AddProduct from "../addProduct/addProduct";
 import "./Body.css";
+import ModalForm from "../modalForm/modalForm";
 
 const Body = () => {
   return (
     <div className="bodyWrapper">
+      <ModalForm />
       <h1 className="text-3xl font-bold underline">Sistema de gerenciamento de produtos</h1>
       <ProductList />
       <AddProduct />
