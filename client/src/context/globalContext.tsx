e9b2a8c41e5a136dd58d08075948cf3718d4d41a
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-10-03 18:04:42 -0300

    feat: globalContext manages product fetch

--- client/src/context/globalContext.tsx
+++ client/src/context/globalContext.tsx
@@ -1,8 +1,11 @@
 import React, { createContext, ReactNode, useState } from "react";
+import { product } from "../types/product";
 
 type GlobalContextTypes = {
+  products: product[],
   isCreating: boolean,
   isEditing: boolean,
+  setProducts: React.Dispatch<React.SetStateAction<product[]>>,
   setIsEditing: React.Dispatch<React.SetStateAction<boolean>>,
   setIsCreating: React.Dispatch<React.SetStateAction<boolean>>,
 }
@@ -16,10 +19,13 @@ export const globalContext = createContext<GlobalContextTypes>(null!);
 const GlobalContext = (props: GlobalProps) => {
   const [isEditing, setIsEditing] = useState<boolean>(false);
   const [isCreating, setIsCreating] = useState<boolean>(false);
+  const [products, setProducts] = useState<product[]>([]);
 
   const globalValue = {
+    products,
     isEditing,
     isCreating,
+    setProducts,
     setIsCreating,
     setIsEditing,
   }
