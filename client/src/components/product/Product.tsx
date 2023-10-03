8654b958c35eeb014ef22469c020ad4a15a38e2d
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-10-02 23:22:31 -0300

    feat: product element

---
+++ client/src/components/product/Product.tsx
@@ -0,0 +1,35 @@
+import { product } from "../../types/product";
+import "./Product.css";
+
+const Product = (props: product) => {
+  const {discountRate, image, name, price} = props;
+
+  function calcPrice() {
+
+    if(discountRate) {
+      return (price - ((discountRate / 100) * price)).toFixed(2).replace(".", ",");
+    }
+
+    return price;
+  }
+
+  return (
+    <div className={`productWrapper ${discountRate ? "withDiscount" : ""}`}>
+      <div className="productImage">
+        <img src={image} alt="Product" />
+      </div>
+
+      <section className="productPriceSection">
+        {discountRate && <p className="productOriginalPrice">{`$${price.toFixed(2).replace(".", ",")}`}</p>}
+        <div>
+          <h3 className="price">{`R$ ${calcPrice()}`}</h3>
+          <h5 className="discountRate">{discountRate ? `${discountRate}% OFF` : null}</h5>
+        </div>
+      </section>
+
+      <h5>{name}</h5>
+    </div>
+  );
+}
+
+export default Product;
