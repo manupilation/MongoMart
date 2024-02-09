b1409c9e54f076a3319ab87539baf2cd1d088bb4
manupilationdev <manupilation.dev@gmail.com>
2024-02-09 00:58:09 -0300

    feat: categories sets new products

--- client/src/components/header/HeaderCategories.tsx
+++ client/src/components/header/HeaderCategories.tsx
@@ -1,11 +1,24 @@
+import { useContext } from "react";
 import { category } from "../../types/product";
+import { globalContext } from "../../context/globalContext";
+import { url } from "../../config/url";
 
 const HeaderCategory = (props: category) => {
   const {id, name} = props;
+  const {setUserProducts} = useContext(globalContext);
+
+  const handleSearchByCategory = async (event: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+    event.preventDefault();
+    const fetchProducts = await fetch(url.backend + `ml/product/category/${id}`);
+    const {products} = await fetchProducts.json();
+    
+    setUserProducts([]);
+    setUserProducts(products);
+  }
 
   return (
-    <li>
-      {name}
+    <li onClick={(event) => handleSearchByCategory(event)}>
+      <a>{name}</a>
     </li>
   );
 }
