df0e0fa686bb038725f8917c09edcb729292dc29
manupilationdev <manupilation.dev@gmail.com>
2024-02-16 20:26:13 -0300

    chore:move route logic to app route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,8 @@
-import { useContext } from "react";
-import UserBody from "./components/UserBody/UserBody";
-import Body from "./components/body/Body";
-import Header from "./components/header/Header";
-import { globalContext } from "./context/globalContext";
-import UserHeader from "./components/header/UserHeader";
+import AppRoute from "./router/App.route";
 
 function App() {
-  const { isUser } = useContext(globalContext);
-
-  if (!isUser) {
-    return (
-      <AdmApp />
-    )
-  }
-
   return (
-    <UserApp />
-  );
-}
-
-function UserApp() {
-  return (
-    <main>
-      <UserHeader />
-      <UserBody /> :
-    </main>
-  );
-}
-
-function AdmApp() {
-  return (
-    <main>
-      <Header />
-      <Body />
-    </main>
+    <AppRoute />
   );
 }
 
---
+++ client/src/router/App.route.tsx
@@ -0,0 +1,44 @@
+import { 
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
+import UserHeader from "../components/header/UserHeader";
+import UserBody from "../components/UserBody/UserBody";
+import Header from "../components/header/Header";
+import Body from "../components/body/Body";
+import { globalContext } from "../context/globalContext";
+import { useContext } from "react";
+import UserProductPage from "../components/UserProductPage/UserProductPage";
+
+function UserApp() {
+  return (
+    <main>
+      <UserBody /> :
+    </main>
+  );
+}
+
+function AdmApp() {
+  return (
+    <main>
+      <Body />
+    </main>
+  );
+}
+
+const AppRoute = () => {
+  const { isUser } = useContext(globalContext);
+  
+  return (
+    <BrowserRouter>
+      {isUser ? <UserHeader /> : <Header/>}
+      <Routes >
+        <Route path="/" element={isUser ? <UserApp /> : <AdmApp />} />
+        <Route path="/product/:id" element={<UserProductPage />}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AppRoute;
